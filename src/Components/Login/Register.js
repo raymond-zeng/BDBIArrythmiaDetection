e31ea33657bb73960fd13cff9447064c726daa13
Xinxinidea <87244651+Xinxinidea@users.noreply.github.com>
2024-10-10 13:20:38 -0400

    Added some css

--- src/Components/Login/Register.js
+++ src/Components/Login/Register.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './Login.css';
 
 function Registration() {
     return (
-    <div className = "form">
-        <div className = "username">
-            <label>Username</label>
-            <input type = "text"/>
-            <label>Password</label>
-            <input type ="text"/>
-        </div>
-        <div>
-            <button type="submit">Register</button>
+    <div className="container">
+        <div className = "form">
+            <div className = "username">
+                <label>Username</label>
+                <input type = "text"/>
+                <label>Password</label>
+                <input type ="text"/>
+            </div>
+            <div>
+                <button type="submit">Register</button>
+            </div>
         </div>
     </div>
     )
